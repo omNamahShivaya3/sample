ae58093386cdbe1eec14b6c06271d4ae4b56e302
omNamahShivaya3 <121163296+omNamahShivaya3@users.noreply.github.com>
2024-07-29 09:42:49 +0000

    1512

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    int n, k;
+    vector<int> temp;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        if (i%2 == 1) {
+            temp.push_back(i);
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            temp.push_back(i);
+        }
+    }
+    cout << temp.at(k - 1) << endl;
+
+    return 0;
+}
