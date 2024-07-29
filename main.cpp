146f1d6e77f2be546190b555e9a2a97569ff7610
omNamahShivaya3 <121163296+omNamahShivaya3@users.noreply.github.com>
2024-07-29 09:49:00 +0000

    1518

--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int main () {
     long long n, k;
-    vector<int> temp;
+    vector<long long> temp;
     cin >> n >> k;
-    for (int i = 1; i <= n; i++) {
+    for (long long i = 1; i <= n; i++)
+    {
         if (i%2 == 1) {
             temp.push_back(i);
         }
     }
-    for (int i = 1; i <= n; i++)
+    for (long long i = 1; i <= n; i++)
     {
         if (i % 2 == 0)
         {
