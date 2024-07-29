6e674b361e0b39fc710c2c2256c9a1cc3adef95d
omNamahShivaya3 <121163296+omNamahShivaya3@users.noreply.github.com>
2024-07-29 10:08:38 +0000

    1538

--- main.cpp
+++ main.cpp
@@ -4,22 +4,14 @@ using namespace std;
 
 int main () {
     long long n, k;
-    vector<long long> temp;
     cin >> n >> k;
-    for (long long i = 1; i <= n; i++)
-    {
-        if (i%2 == 1) {
-            temp.push_back(i);
-        }
-    }
-    for (long long i = 1; i <= n; i++)
-    {
-        if (i % 2 == 0)
-        {
-            temp.push_back(i);
-        }
-    }
-    cout << temp.at(k - 1) << endl;
+    long long num_odd = (n + 1) / 2;
 
+    if (k <= num_odd) {
+        cout << 2 * k - 1 << endl;
+    }
+    else {
+        cout << 2 * (k - num_odd) << endl;
+    }
     return 0;
 }
