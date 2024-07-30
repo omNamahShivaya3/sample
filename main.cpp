3babba80f5bd4fa3d136e7d4e2ca0516fdec4aab
omNamahShivaya3 <121163296+omNamahShivaya3@users.noreply.github.com>
2024-07-30 09:01:33 +0000

    1431

--- main.cpp
+++ main.cpp
@@ -1,17 +1,12 @@
+// Here is a simple and short C++ code to solve the problem : C++
+
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int main () {
-    long long n, k;
-    cin >> n >> k;
-    long long num_odd = (n + 1) / 2;
-
-    if (k <= num_odd) {
-        cout << 2 * k - 1 << endl;
-    }
-    else {
-        cout << 2 * (k - num_odd) << endl;
-    }
+int main()
+{
+    int x;
+    cin >> x;
+    cout << (x + 4) / 5 << endl;
     return 0;
 }
