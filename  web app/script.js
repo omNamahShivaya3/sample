85f3ef100d9479bf6af9090c991c06409bfa758c
omNamahShivaya3 <121163296+omNamahShivaya3@users.noreply.github.com>
2024-08-01 18:09:53 +0000

    2339

---
+++  web app/script.js
@@ -0,0 +1,17 @@
+// objects in JS
+let phone = {
+    name : "Galaxy Pro", 
+    model : "SM-G7102"
+}
+console.log("The phone name is " + phone.name);
+console.log("The phone model is " + phone.model);
+
+// Arrays in JS
+let phones = ["Galaxy", "Nokia", "Redmi"];
+console.log("The array elements are: " + phones[0] + " " + phones[1] + " " + phones[2]);
+
+// functions in JS
+function cube (number) {
+    return (number*number*number);
+}
+console.log(cube(5));
